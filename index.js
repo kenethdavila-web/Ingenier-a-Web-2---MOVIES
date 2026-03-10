f8de32b310ed1a1e94578dfb69f6c462634bb091
kenethdavila-web <keneth.davila@est.iudigital.edu.co>
2026-03-09 23:51:23 -0500

    modificación del index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const {getConnection} = require("./db/db-connection-mongo");
+const {getConnection} = require("./backend/db/db-connection-mongo");
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -11,11 +11,11 @@ app.use(cors());
 app.use(express.json());
 
 /*-----Routes -----*/
-app.use("/api/genero" , require("./routes/genero"));
-app.use("/api/media", require("./routes/media"));
-app.use("/api/director", require("./routes/director"));
-app.use("/api/productora", require("./routes/productora"));
-app.use("/api/tipo", require("./routes/tipo"));
+app.use("/api/genero", require("./backend/routes/genero"));
+app.use("/api/media", require("./backend/routes/media"));
+app.use("/api/director", require("./backend/routes/director"));
+app.use("/api/productora", require("./backend/routes/productora"));
+app.use("/api/tipo", require("./backend/routes/tipo"));
 
 getConnection();
 
